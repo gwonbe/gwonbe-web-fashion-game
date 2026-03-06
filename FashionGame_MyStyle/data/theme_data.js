a323f4cf7739fec637e9615ef80a230e1b7c0983
Shin Yujin <108513967+gwonbe@users.noreply.github.com>
2026-03-06 21:52:40 +0900

    [2026.03.06.금] : 아이템 추가

--- FashionGame_MyStyle/data/theme_data.js
+++ FashionGame_MyStyle/data/theme_data.js
@@ -1,8 +1,8 @@
 export const THEME_DATA = [
     {
-        id: "Cozy Pink 꿈나라",
+        id: "Cozy Pink Dream",
         name: "theme_001",
         thumb: "assets/theme/theme_001.png",
-        items: ["dress_001.png", "dress_002.png", "shoes_002.png",]
+        items: ["dress_001.png", "dress_002.png", "shoes_002.png", "shoes_003.png", "hair-band_001.png"]
     },
 ];
